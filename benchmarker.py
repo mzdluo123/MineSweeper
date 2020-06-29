d20ee203016a099f6df2a16ec6776078b15e5ceb
mzdluo123 <mzdluo123@foxmail.com>
2020-06-29 12:58:05 +0800

    add benchmaark

---
+++ benchmarker.py
@@ -0,0 +1,9 @@
+import time
+from minesweeper import MineSweeper
+
+if __name__ == '__main__':
+    start = time.time()
+    for i in range(0, 300):
+        mine = MineSweeper(25, 25, 25)
+        mine.mine(5, 10)
+    print(time.time() - start)
